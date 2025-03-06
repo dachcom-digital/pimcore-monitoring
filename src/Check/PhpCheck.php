b09befa1c743fe7634f866937fa7e6cbe423fc2b
Marcel Zweifel <mzweifel@dachcom.ch>
2025-03-06 12:07:38 +0100

    Feature: Check PHP and Kernel (#13)

---
+++ src/Check/PhpCheck.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This source file is available under two different licenses:
+ *   - GNU General Public License version 3 (GPLv3)
+ *   - DACHCOM Commercial License (DCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) DACHCOM.DIGITAL AG (https://www.dachcom-digital.com)
+ * @license    GPLv3 and DCL
+ */
+
+namespace MonitoringBundle\Check;
+
+class PhpCheck implements CheckInterface
+{
+    public function getCheckReportIdentifier(): string
+    {
+        return 'php';
+    }
+
+    public function getCheckReport(): array
+    {
+        return [
+            'version'             => PHP_VERSION,
+            'memory_limit'        => ini_get('memory_limit'),
+            'max_execution_time'  => ini_get('max_execution_time'),
+            'upload_max_filesize' => ini_get('upload_max_filesize'),
+            'post_max_size'       => ini_get('post_max_size'),
+        ];
+    }
+}
